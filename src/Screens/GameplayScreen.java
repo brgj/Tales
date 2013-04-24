8771fd190a0c0f974235c8db3467b0849dfa8fdc
Dylan <fall_back_up@hotmail.com>
2013-04-24 01:14:41 -0700

    Camera

--- src/Screens/GameplayScreen.java
+++ src/Screens/GameplayScreen.java
@@ -1,8 +1,12 @@
 package Screens;
 
+import core.Camera;
 import helpers.Delegate;
+import org.lwjgl.input.Keyboard;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.util.glu.GLU;
+import org.lwjgl.util.vector.Vector3f;
 
 import java.util.ArrayList;
 
@@ -17,8 +21,12 @@ import static org.lwjgl.opengl.GL11.*;
  */
 public class GameplayScreen extends Screen
 {
+    //Camera for single player
+    Camera cam;
+
     public GameplayScreen(Delegate d) {
         super(d);
+        cam = new Camera(new Vector3f(0, 0, 0));
     }
 
     public void Initialize(){
@@ -30,18 +38,65 @@ public class GameplayScreen extends Screen
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         glColor3f(0.0f, 1.0f, 1.0f);
 
+        glRotatef(0.0f, 0.0f, 0.0f, 1.0f);
+        glDisable(GL_DEPTH_TEST);
+
         glBegin(GL_QUADS);
 
-        glVertex2f(100,100);
-        glVertex2f(100+600,100);
-        glVertex2f(100+600,100+400);
-        glVertex2f(100,100+400);
+        glColor3d(1, 0, 0);
+        glVertex3i(50, 50, 50);
+        glVertex3i(50, -50, 50);
+        glVertex3i(-50, -50, 50);
+        glVertex3i(-50, 50, 50);
+
+
+        glColor3d(0, 1, 0);
+        glVertex3i(50, 50, -50);
+        glVertex3i(50, -50, -50);
+        glVertex3i(-50, -50, -50);
+        glVertex3i(-50, 50, -50);
+
+
+        glColor3d(0, 0, 1);
+        glVertex3i(50, 50, 50);
+        glVertex3i(50, -50, 50);
+        glVertex3i(50, -50, -50);
+        glVertex3i(50, 50, -50);
+
+
+        glColor3d(0, 1, 1);
+        glVertex3i(-50, 50, 50);
+        glVertex3i(-50, -50, 50);
+        glVertex3i(-50, -50, -50);
+        glVertex3i(-50, 50, -50);
+
+
+        glColor3d(1, 0, 0);
+        glVertex3i(-50, 50, -50);
+        glVertex3i(-50, 50, 50);
+        glVertex3i(50, 50, 50);
+        glVertex3i(50, 50, -50);
+
+
+        glColor3d(1, 1, 0);
+        glVertex3i(-50, -50, -50);
+        glVertex3i(-50, -50, 50);
+        glVertex3i(50, -50, 50);
+        glVertex3i(50, -50, -50);
 
         glEnd();
+        cam.setCameraView();
 
     }
 
     public void Update(){
-
+        if(Keyboard.isKeyDown(Keyboard.KEY_UP))
+        {
+            cam.walk(1);
+        }
+        if(Keyboard.isKeyDown(Keyboard.KEY_DOWN))
+        {
+            cam.rwalk(1);
+        }
     }
 }
---
+++ src/core/Camera.java
@@ -0,0 +1,108 @@
+package core;
+
+import org.lwjgl.util.vector.Vector3f;
+import static org.lwjgl.opengl.GL11.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Owner
+ * Date: 23/04/13
+ * Time: 9:02 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Camera {
+
+    //The position that the camera will be placed in the world
+    private Vector3f position;
+    //Rotation around Y axis
+    private float yaw;
+    //Rotation around X axis
+    private float pitch;
+    //Rotation around Z axis
+    private float roll;
+
+    //Constructor that takes a vector
+    public Camera(Vector3f initialPosition)
+    {
+        position = initialPosition;
+        yaw = 0.0f;
+        pitch = 0.0f;
+        roll = 0.0f;
+        //Set initial camera view
+        setCameraView();
+    }
+
+    /*
+    TODO: Implement following of the player. This will have to be implemented when we figure out how the camera will transform as it follows the ship
+     */
+
+    //Changes the cameras view based on the position, yew, and pitch
+    public void setCameraView()
+    {
+
+        glEnable(GL_DEPTH_TEST);
+        glLoadIdentity();
+        //Sets the cameras X rotation
+        glRotatef(pitch, 1.0f, 0.0f, 0.0f);
+        //Sets the cameras Y rotation
+        glRotatef(yaw, 0.0f, 1.0f, 0.0f);
+        //Sets the cameras Z rotation
+        glRotatef(roll, 0.0f, 0.0f, 1.0f);
+        //Sets the camera to a position
+        glTranslatef(position.x, position.y, position.z);
+    }
+
+    public void walk(float units)
+    {
+        position.x -= units * (float)Math.sin(Math.toRadians(yaw));
+        position.z += units * (float)Math.cos(Math.toRadians(yaw));
+    }
+
+    public void rwalk(float units)
+    {
+        position.x += units * (float)Math.sin(Math.toRadians(yaw));
+        position.z -= units * (float)Math.cos(Math.toRadians(yaw));
+    }
+
+    public void strafe(float units)
+    {
+
+    }
+
+
+    //region Getters and Setters
+
+    public Vector3f getPosition() {
+        return position;
+    }
+
+    public void setPosition(Vector3f position) {
+        this.position = position;
+    }
+
+    public float getYaw() {
+        return yaw;
+    }
+
+    public void setYaw(float yaw) {
+        this.yaw += yaw;
+    }
+
+    public float getPitch() {
+        return pitch;
+    }
+
+    public void setPitch(float pitch) {
+        this.pitch += pitch;
+    }
+
+    public float getRoll() {
+        return roll;
+    }
+
+    public void setRoll(float roll) {
+        this.roll = roll;
+    }
+
+    //endregion
+}
