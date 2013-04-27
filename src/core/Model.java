7c8855c216cbf1c56eed9a1e0f4e6c131d8678d7
Tammy H <tamhoang2806@gmail.com>
2013-04-26 19:54:56 -0700

    added some low poly models

---
+++ src/core/Model.java
@@ -0,0 +1,11 @@
+package core;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Tammy
+ * Date: 4/26/13
+ * Time: 10:42 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Model {
+}
