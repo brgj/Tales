1ecb0b8e2c97aed7e9aa20bc5da6c3deee750f90
Dylan <fall_back_up@hotmail.com>
2013-05-02 11:03:48 -0700

    Entity class structure

---
+++ src/entity/Entity.java
@@ -0,0 +1,41 @@
+package entity;
+
+import environment.Model;
+import org.lwjgl.util.vector.Vector3f;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Owner
+ * Date: 02/05/13
+ * Time: 10:53 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public abstract class Entity {
+
+    Vector3f position;
+    Model model;
+
+    public Entity()
+    {
+
+    }
+
+    public void Render()
+    {
+
+    }
+
+    public void Update()
+    {
+
+    }
+
+    public void Initialize()
+    {
+
+    }
+
+
+
+
+}
---
+++ src/entity/Player.java
@@ -0,0 +1,31 @@
+package entity;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Owner
+ * Date: 02/05/13
+ * Time: 10:50 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Player extends Entity {
+
+    public Player()
+    {
+
+    }
+
+    public void Render()
+    {
+
+    }
+
+    public void Update()
+    {
+
+    }
+
+    public void Initialize()
+    {
+
+    }
+}
